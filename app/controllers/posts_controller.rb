56cc49e9ae79fc6fff520b8281351ba5cada8e8b
techyuri <89583984+techyuri@users.noreply.github.com>
2021-09-08 12:50:42 +0900

    heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,11 @@ class PostsController < ApplicationController
   end
 
   def create
+<<<<<<< Updated upstream
     Post.create(content: params[:content])
+=======
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+>>>>>>> Stashed changes
   end
 end
